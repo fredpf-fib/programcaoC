3c6b20cbcff9a8847d0db6c894d5f2f18c1d7df9
fredpf-fib <202503139385@alunos.estacio.br>
2025-04-16 00:54:00 +0000

    Código para ser enviado no desafio

---
+++ Super Trunfo/super_trunfo.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+int main(){
+
+    char estado[30], codigo[3], cidade[50];
+    int populacao, pontosTuristicos;
+    float area, pib;
+
+//Obter informações da carta 1
+    printf("Por favor, digitar as informações da Carta 1: \n\n");
+
+    printf("Digite o Estado: \n");
+    scanf("%s",&estado);
+
+    printf("Digite o código da carta: \n");
+    scanf("%s",&codigo);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s" ,&cidade);
+
+    printf("Digite a população: \n");
+    scanf("%d",&populacao);
+
+    printf("Digite a área: \n");
+    scanf("%f",&area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f",&pib);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d",&pontosTuristicos);
+    printf("\n");
+ 
+//Exibir informações da carta 1
+    printf("=================================================\n");
+    printf("==========           Carta 1           ==========\n");
+    printf("=================================================\n\n");
+    printf("Estado: %s \n",estado);
+    printf("Código: %s \n",codigo);
+    printf("Nome da Cidade: %s \n",cidade);
+    printf("População: %d \n",populacao);
+    printf("Área: %f \n",area);
+    printf("PIB: %f \n",pib);
+    printf("Número de Pontos Turísticos: %d \n\n",pontosTuristicos);
+    printf("=================================================\n\n");
+    
+//Obter informações da carta 2
+    printf("Por favor digitar as informações da Carta 2: \n\n");
+
+    printf("Digite o Estado: \n");
+    scanf("%s",&estado);
+
+printf("Digite o código da carta: \n");
+scanf("%s",&codigo);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s" ,&cidade);
+
+printf("Digite a população: \n");
+scanf("%d",&populacao);
+
+printf("Digite a área: \n");
+scanf("%f",&area);
+
+printf("Digite o PIB: \n");
+scanf("%f",&pib);
+
+printf("Digite o número de pontos turísticos: \n");
+scanf("%d",&pontosTuristicos);
+printf("\n");
+
+//Exibir informações da carta 2
+printf("=================================================\n");
+printf("==========           Carta 2           ==========\n");
+printf("=================================================\n\n");
+printf("Estado: %s \n",estado);
+printf("Código: %s \n",codigo);
+printf("Nome da Cidade: %s \n",cidade);
+printf("População: %d \n",populacao);
+printf("Área: %f \n",area);
+printf("PIB: %f \n",pib);
+printf("Número de Pontos Turísticos: %d \n\n",pontosTuristicos);
+printf("=================================================\n");
+
+/*
+    printf("Nome do aluno: %s - Matrícula: %d \n",nome ,matricula);
+    printf("Idade: %d - Altura: %f",idade ,altura);
+*/
+    return 0;
+
+}
